be84d3ae64145f7b64898b048d49cfa0bdf12c4a
Theorc1979 <30360107+Theorc1979@users.noreply.github.com>
2017-07-21 21:51:17 -0500

    primera subida
    presentacion

---
+++ app/src/main/java/hector_corredor/myproyectoa/MainActivity.java
@@ -0,0 +1,13 @@
+package hector_corredor.myproyectoa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
